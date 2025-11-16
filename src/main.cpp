d511e4a2959396f3dd2b4b50b3f19b123f8587ea
joaquinpco <joaquin1208@me.com>
2025-11-16 22:30:29 +0100

    feat: Refactor main class without code for Framework entry point. Added multiplication and transpose matrix test. Added vector constructor, getters for rows/cols and transpose method.

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,5 @@ using namespace cpplearn;
 
 int main()
 {
-    cpplearn::Matrix A(2, 2, 1.0);
-    cpplearn::Matrix B(2, 2, 2.0);
-
-    auto C = A + B;
-    auto D = A * B;
-
-    std::cout << "Matrix C (A + B):\n";
-    C.show();
-
-    std::cout << "\nMatrix D (A * B):\n";
-    D.show();
-
     return 0;
 }
